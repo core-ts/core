0695319e2da515d1c835c99a0f6be7bcf3844d00
Duc Nguyen <minhduc140583@gmail.com>
2020-10-25 16:24:48 +0700

    Add DiffList, ApprList

--- src/service/ApprService.ts
+++ src/service/ApprService.ts
@@ -9,7 +9,16 @@ export interface ApprService<ID> {
   approve(id: ID, ctx?: any): Promise<Status>;
   reject(id: ID, ctx?: any): Promise<Status>;
 }
+export interface ApprListService<ID> {
+  approve(ids: ID[], ctx?: any): Promise<Status>;
+  reject(ids: ID[], ctx?: any): Promise<Status>;
+}
+
 export interface ApprRepository<ID> {
   approve(id: ID, ctx?: any): Promise<Status>;
   reject(id: ID, ctx?: any): Promise<Status>;
+}
+export interface ApprListRepository<ID> {
+  approve(ids: ID[], ctx?: any): Promise<Status>;
+  reject(ids: ID[], ctx?: any): Promise<Status>;
 }
--- src/service/DiffService.ts
+++ src/service/DiffService.ts
@@ -5,7 +5,16 @@ export interface DiffService<T, ID> {
   keys(): string[];
   diff(id: ID, ctx?: any): Promise<DiffModel<T, ID>>;
 }
+export interface DiffListService<T, ID> {
+  keys(): string[];
+  diff(ids: ID[], ctx?: any): Promise<Array<DiffModel<T, ID>>>;
+}
+
 export interface DiffRepository<T, ID> {
   keys(): string[];
   diff(id: ID, ctx?: any): Promise<DiffModel<T, ID>>;
+}
+export interface DiffListRepository<T, ID> {
+  keys(): string[];
+  diff(ids: ID[], ctx?: any): Promise<Array<DiffModel<T, ID>>>;
 }

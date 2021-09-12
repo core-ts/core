475d14bec0f0b1fa448c1918f279d72cbb9f5df2
Duc Nguyen <minhduc140583@gmail.com>
2021-09-12 09:26:25 +0700

    refactor search

--- src/repository/SearchRepository.ts
+++ src/repository/SearchRepository.ts
@@ -2,5 +2,5 @@ import {SearchModel} from '../model';
 import {SearchResult} from '../model';
 
 export interface SearchRepository<T, S extends SearchModel> {
-  search(s: S, limit?: number, offset?: number|string, fields?: string[], ctx?: any): Promise<SearchResult<T>|T[]>;
+  search(s: S, limit?: number, offset?: number|string, fields?: string[], ctx?: any): Promise<SearchResult<T>>;
 }

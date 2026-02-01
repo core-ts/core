f0b3ced965e6ec278bfd5b32327e7f0859550eba
Duc Nguyen <minhduc140583@gmail.com>
2026-02-01 07:58:00 +0700

    Refactor DB API

--- src/db.ts
+++ src/db.ts
@@ -13,10 +13,18 @@ export interface SimpleDB {
 interface StringMap {
   [key: string]: string;
 }
-export interface DB {
+export interface Executor {
   driver: string;
   param(i: number): string;
   exec(sql: string, args?: any[], ctx?: any): Promise<number>;
   execBatch(statements: Statement[], firstSuccess?: boolean, ctx?: any): Promise<number>;
   query<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T[]>;
+}
+export interface Transaction extends Executor {
+  commit(): Promise<void>
+  rollback(): Promise<void>
+  end(): Promise<void>
+}
+export interface DB extends Executor {
+  beginTransaction(): Promise<Transaction>
 }
